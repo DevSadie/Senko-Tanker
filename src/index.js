dfd89975651a521c06c851de3e5938676d77915d
Laura Breckams <SenkoDev@gmail.com>
2021-02-06 10:32:39 +0300

    add promise rejection handler

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
 const { discordToken } = require('./config/secrets.json');
-const { green } = require('chalk');
-
+const { green, red } = require('chalk');
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.cooldown = new Discord.Collection();
-
 client.config = require('./config/config.json');
 client.colors = require('./config/colors.json');
 
 ['commandController', 'eventController'].forEach(controller => {
 	require(`./controllers/${controller}`)(client, Discord);
 });
+
+process.on("unhandledRejection", e => console.error(`${red('ERROR')} ${e}`));
 
 client.on('ready', () => {
 	client.user.setActivity('chat.',
