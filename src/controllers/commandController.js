6be4e57a04096cdaa32b7823df7b3af27b1ce697
Sadie K. Breckams <senkodev@gmail.com>
2021-03-18 10:56:20 +0300

    Will it work?

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -5,6 +5,7 @@ module.exports = (client, Discord) => {
     // read command files
     const commandFiles = fs.readdirSync(`${client.root}/commands`).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
+        
         // require command
         const command = require(`${client.root}/commands/${file}`);
         
